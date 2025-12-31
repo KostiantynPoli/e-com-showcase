dfc0ace89a66f561570a48d6217de73fcd0b2587
Kostiantyn Polishchuk <kostiantyn@Kostiantyns-MacBook-Pro.local>
2025-12-31 13:38:38 +0100

    update nuxt config offer-Per-page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 export default defineNuxtConfig({
     // Runtime Config
     runtimeConfig: {
-        public: {
-            offerPerPage: Number(process.env.OFFER_PER_PAGE) || 10,
-        },
         salesDoublerBaseUrl: process.env.SALES_DOUBLER_BASE_URL,
         salesDoublerToken: process.env.AFFILIATE_API_KEY,
         salesDoublerCategory: Number(process.env.E_COM_CATEGORY_ID),
+        offerPerPage: Number(process.env.OFFER_PER_PAGE),
     },
 
     // Модули
