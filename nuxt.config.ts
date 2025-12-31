4824a9a1edbd9be9c72de34d9e33fece030beae0
Kostiantyn Polishchuk <kostiantyn@Kostiantyns-MacBook-Pro.local>
2025-12-31 13:24:56 +0100

    add css config in the nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 export default defineNuxtConfig({
     runtimeConfig: {
+        public: {
+            offerPerPage: Number(process.env.OFFER_PER_PAGE) || 10
+        },
         salesDoublerBaseUrl: process.env.SALES_DOUBLER_BASE_URL,
         salesDoublerToken: process.env.AFFILIATE_API_KEY,
         salesDoublerCategory: Number(process.env.E_COM_CATEGORY_ID),
-        offerPerPage: Number(process.env.OFFER_PER_PAGE),
+
     },
 
     modules: [
         '@nuxtjs/tailwindcss',
+    ],
+    css: [
+        '~/assets/css/tailwind.css', // если есть кастомный css
     ],
 
     compatibilityDate: '2025-12-28',
